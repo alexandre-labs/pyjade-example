069628bb92f3ff0212c1e3e33e1bb0ba29b1d68c
Alexandre Souza <aesouza.jvm@gmail.com>
2014-10-19 14:35:59 -0200

    adicionando comentários

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from app import app
 
+# exibe erros
 app.debug = True
+
+# recarrega os arquivos quando houver alterações
 app.use_reload = True
+
+# faz a mágica, extensão .jade
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 if __name__ == '__main__':
